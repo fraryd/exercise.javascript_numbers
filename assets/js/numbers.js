8976d82d399ad91d4561405762446129bb37a799
David Frary <fraryd@gmail.com>
2020-07-06 13:42:50 -0400

    finished edits on 'numbers.js'

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -2,27 +2,23 @@
 
 function compute(expression) {
     
-let userInput1 = "";
-let userInput2 = "";    
-// let Operator = "";
-let sum = "";
-// let Equals = "";
-
+addition();
+subtraction();
 
 //Addition
 function addition(num1, num2){
-    
-    for (let i=0; i<num1; i++){
-        userInput1 += 1;
-    }
-    for(let i=0; i<num2; i++){
-        userInput2 += 1;
-        userInput2 += userInput2;
-       
-    }
-userInput2 += userInput1;
+    num1=1;
+    num2=1;
+    return num1+num2;
 }
 
-addition();
-return userInput2;
+//Subtraction
+function subtraction(num1, num2){
+    num1=1;
+    num2=1;
+    return num1-num2;
 }
+
+return subtraction();
+}
+
