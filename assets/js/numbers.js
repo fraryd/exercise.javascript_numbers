09b83ece73ec48dbeb1b6e597ef7ff3eb876d625
David Frary <fraryd@gmail.com>
2020-07-06 19:45:20 -0400

    finished edits on 'numbers.js'

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -9,9 +9,17 @@ division();
 
 //Addition
 function addition(num1, num2){
-    num1=1;
-    num2=1;
-    return num1+num2;
+    num1 = 1;
+    num2 = 1;
+    sum = 0;
+
+    for (let i=0; i<num1; i++){
+    }
+    for (let i=0; i<num2; i++){
+    }
+
+    sum = num1+num2;
+    return sum;
 }
 
 //Subtraction
@@ -37,6 +45,5 @@ function division(num1, num2){
     return num1/num2;
 }
 
-return division();
+return addition();
 }
-
